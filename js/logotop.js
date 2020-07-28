5503ed48035a2b46e1a780f3bdba07f839d33fa8
けろ <65333609+kero-ugok@users.noreply.github.com>
2020-07-28 16:14:04 +0900

    つくりました

--- js/logotop.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-   $(window).scroll(function(){
-      if($(window).scrollTop($('.container').offset()top)) {
-         $('.logo img').animate({ width: +=200});
-         
-      }
-   })
-});
