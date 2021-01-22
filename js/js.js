ba6b5abb3f9f2455b4f5045259bda79326e43e22
けろ <65333609+kero-ugok@users.noreply.github.com>
2021-01-22 23:59:06 +0900

    どーん

---
+++ js/js.js
@@ -0,0 +1,132 @@
+let product = {
+  uebxli: {
+    leftCaption: '<h3><a href="https://uebxli.github.io/" target="_brank">UebXli紹介サイト</a></h3><h4>ウェブを自主的に学びたい学生たちを繋いで応援するオンラインサークル</h4><p>ゼミ活動の一環として、Web制作応援サークルを作りました。このサイトは、そのメンバー（同学年）と初めて共同制作したサイトです。私はヘッダー・トップとJavaScriptを担当しています。GitHubを使用して制作しましたが、意思の疎通はもちろん、足並みを揃えるということの難しさを痛感しました。JSプラグインはGSAPを使用しています。</p>',
+    rightTags: '<div class="tag tag-notalone">共同制作</div><div class="tag tag-htmlcss">HTML5 / CSS3</div><div class="tag tag-js">JavaScript</div>',
+    Date: '2020/05 ~ 2020/06'
+  },
+  superpenguin: {
+    leftCaption: '<h3><a href="SUPER_PENGUIN/index.html" target="_brank">SUPER PENGUIN -Official Site-</a></h3><h4>架空バンドグループ公式サイト</h4><p>ウェブデザイン演習という授業で制作した作品です。初めてデザインカンプから制作し、背景に文字や図形を入れるなど、デザイン志向になるようにしました。ロゴやバンド設定は全て自分で制作しています。ディスコグラフィーページでの画像の使い方に苦労しましたが、納得のいくデザインにはなりませんでした。CSSフレームワークはBootstrapを使用しています。人物画像はiStockです。</p>',
+    rightTags: '<div class="tag tag-alone">個人制作</div><div class="tag tag-htmlcss">HTML5 / CSS3</div><div class="tag tag-js">JavaScript</div><div class="tag tag-cssframe">CSSフレームワーク</div><div class="tag tag-illustrator">Adobe Illustrator</div>',
+    Date: '2020/07 ~ 2020/08',
+  },
+  tsuri: {
+    leftCaption: '<h3><a href="tsuri/index.html" target="_brank">魚釣りゲーム</a></h3><h4>Canvasを使用した魚釣りゲーム</h4><p>ウェブアプリケーション論という授業で制作した作品です。初めてCanvasを使用して制作しました。画像は使用しておらず、JavaScriptのみで動かしています。制作当初はバグが多く、デバッグ作業で一日が終わる日々でした。唯一治せなかったのが背景の海と魚を別々に動かしているため、魚が点滅してしまうバグです。悔しいです。</p>',
+    rightTags: '<div class="tag tag-alone">個人制作</div><div class="tag tag-htmlcss">HTML5 / CSS3</div><div class="tag tag-js">JavaScript</div>',
+    Date: '2020/07',
+  },
+  totto: {
+    leftCaption: '<h3><a href="https://uebxli.github.io/website-advanced/index.html" target="_brank">猫Cafe トット</a></h3><h4>架空猫カフェ公式サイト</h4><p>Web制作応援サークルにて、他学年と共同で制作したサイトです。ウェブデザイン、ヘッダー・トップを担当しました。全体的に優しい印象になるようデザインしました。反省点は横に伸びたデザインになってしまった点、横に広いウインドウで閲覧するとややくずれてしまう点です。</p>',
+    rightTags: '<div class="tag tag-notalone">共同制作</div><div class="tag tag-htmlcss">HTML5 / CSS3</div><div class="tag tag-js">JavaScript</div>',
+    Date: '2020/08 ~ 2020/09',
+  },
+  ugokradio: {
+    leftCaption: '<h3><a href="https://uebxli.github.io/ugok-radio/index.html" target="_brank">UGOKラジオ紹介サイト</a></h3><h4>UGOK広報ラジオ紹介サイト</h4><p>UebXli紹介サイトを制作したメンバーで制作しました。UGOKとは、分離融合系女子大生のグループです。その広報を行うラジオの紹介サイトです。ウェブデザイン、トップ画像、配信内容部分を担当しています。ラジオの雰囲気に合わせて、明るい印象になるようデザインしました。</p>',
+    rightTags: '<div class="tag tag-notalone">共同制作</div><div class="tag tag-htmlcss">HTML5 / CSS3</div><div class="tag tag-js">JavaScript</div><div class="tag tag-illustrator">Adobe Illustrator</div>',
+    Date: '2020/08 ~ 2020/09',
+  },
+  moppymovie: {
+    leftCaption: '<h3><a href="https://moppymovie.github.io" target="_brank">映画もっぴぃ インターネット大冒険 公式サイト</a></h3><h4>架空映画公式サイト</h4><p>クロスメディア演習という授業で制作しました。この授業ではサイトだけでなく、映画PV・ポスター・劇場入場者特典など、様々な作品を制作しました。映像編集、サイト制作、ポスター、入場者特典、その他素材制作を担当しました。これまでの作品の中で最も制作期間が長く、力を注いだ作品です。動画のリンクは<a href="https://youtu.be/_UdVoSWXPWY" target="_blank">こちら</a>です（Youtube）</p>',
+    rightTags: '<div class="tag tag-notalone">共同制作</div><div class="tag tag-htmlcss">HTML5 / CSS3</div><div class="tag tag-js">JavaScript</div><div class="tag tag-cssframe">CSSフレームワーク</div><div class="tag tag-illustrator">Adobe Illustrator</div><div class="tag tag-photoshop">Adobe Photoshop</div><div class="tag tag-aftereffects">Adobe AfterEffects</div>',
+    Date: '2020/11 ~ 2020/12',
+  },
+  sushi: {
+    leftCaption: '<h3><a href="sushi/index.html" target="_brank">漢字読み取りゲーム 魚・魚・魚</a></h3><h4>魚の漢字読み取りゲーム</h4><p>ウェブアプリケーション設計演習という授業で制作しました。アニメーションはCSS3を使用しており、Vue.jsとの連動を目指しました。効果音を使用しており、よりゲームらしく制作できました。反省点は純粋なJavaScriptに頼りすぎてしまい、Vue.jsを効果的に使えなかった点です。制作日数は4日でした。</p>',
+    rightTags: '<div class="tag tag-alone">個人制作</div><div class="tag tag-htmlcss">HTML5 / CSS3</div><div class="tag tag-js">JavaScript</div><div class="tag tag-vue">Vue.js</div>',
+    Date: '2021/12',
+  }
+}
+
+let productName = ['uebxli', 'superpenguin', 'tsuri', 'totto', 'ugokradio', 'moppymovie', 'sushi']
+
+let topNum = 1;
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    pcTop: 'img/top_' + topNum + '.png',
+    phoneTop: 'img/phone_top_' + topNum + '.png',
+    show: 'hide',
+    img_1: '',
+    img_1_2: '',
+    img_2: '',
+    img_3: '',
+    caption: '',
+    tags: '',
+    date: '',
+    num: ''
+  },
+  methods: {
+    chageTop: function () {
+      setTimeout(() => {
+        if (topNum < 5) {
+          topNum += 1;
+          this.pcTop = 'img/top_' + topNum + '.png';
+          this.phoneTop = 'img/phone_top_' + topNum + '.png';
+        } else {
+          topNum = 1;
+          this.pcTop = 'img/top_' + topNum + '.png';
+          this.phoneTop = 'img/phone_top_' + topNum + '.png';
+        }
+        setTimeout(this.chageTop, 7000);
+      }, 7000);
+    },
+    productChandge: function (num) {
+      this.img_1 = 'img/products/' + productName[this.num] + '_1.jpg';
+      this.img_1_2 = 'img/products/' + productName[this.num] + '_1.jpg';
+      this.img_2 = 'img/products/' + productName[this.num] + '_2.jpg';
+      this.img_3 = 'img/products/' + productName[this.num] + '_3.jpg';
+      this.caption = product[productName[this.num]].leftCaption;
+      this.tags = product[productName[this.num]].rightTags;
+      this.date = product[productName[this.num]].Date;
+    },
+    imgChange_1: function () {
+      this.img_1 = this.img_1_2;
+    },
+    imgChange_2: function () {
+      this.img_1 = this.img_2;
+    },
+    imgChange_3: function () {
+      this.img_1 = this.img_3;
+    },
+    close: function () {
+      this.show = 'hide';
+    },
+    clickUeb: function () {
+      this.num = 0;
+      this.productChandge();
+      this.show = 'show';
+    },
+    clickSp: function () {
+      this.num = 1;
+      this.productChandge();
+      this.show = 'show';
+    },
+    clickTsu: function () {
+      this.num = 2;
+      this.productChandge();
+      this.show = 'show';
+    },
+    clickTo: function () {
+      this.num = 3;
+      this.productChandge();
+      this.show = 'show';
+    },
+    clickUgok: function () {
+      this.num = 4;
+      this.productChandge();
+      this.show = 'show';
+    },
+    clickMoppy: function () {
+      this.num = 5;
+      this.productChandge();
+      this.show = 'show';
+    },
+    clickSushi: function () {
+      this.num = 6;
+      this.productChandge();
+      this.show = 'show';
+    }
+  }
+})
+
+app.chageTop();
